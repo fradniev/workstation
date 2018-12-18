a3084a1fe5ee7e3092411fb506cd63c6a822169e
Fradniev <fradniev@hotmail.com>
2018-12-18 17:39:56 -0300

    added save time and name

--- server.js
+++ server.js
@@ -13,39 +13,53 @@ app.set('view engine', 'html');
  * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
  * and exposes the resulting object (containing the keys and values) on req.body
  */
- app.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
   extended: true
 }));
 
- app.use(bodyParser.json());
+app.use(bodyParser.json());
 
- app.get('/', function (req, res) {
+app.get('/', function (req, res) {
   var markers = JSON.parse(fs.readFileSync('markers.json', 'utf8'));
   res.render('index', {markers:markers});
-})
+});
 
- app.post('/', function (req, res) {
+app.post('/', function (req, res) {
   fs.writeFileSync('markers.json', JSON.stringify(req.body));
   res.send(true);
-})
+});
 
- app.post('/position', function (req, res) {
-  var markers = JSON.parse(fs.readFileSync('markers.json', 'utf8'));
-  for (var j = 0; j< markers.length; j++) {
-    if (geometry.computeDistanceBetween(
-      new geometry.LatLng( markers[j].lat, markers[j].lng ),
-      new geometry.LatLng( req.body.lat, req.body.lng )
-      ) <= 30) {
-      console.log("In Workplace.");
-    break;
-    } else {
-      console.log("Away from Workplace.");
+app.post('/position', function (req, res) {
+  console.log(req.body);
+  if(req.body.pos!=null){
+    var markers = JSON.parse(fs.readFileSync('markers.json', 'utf8'));
+    var j = 0;
+    while (j< markers.length) {
+      if (geometry.computeDistanceBetween(new geometry.LatLng( markers[j].lat, markers[j].lng ),new geometry.LatLng( req.body.pos.lat, req.body.pos.lng )) <= 50){
+        if (req.body.time!=null && req.body.work!=null && req.body.work) {
+          var save="User: "+req.body.user+". In work: "+req.body.time+"\n";
+          fs.appendFile('users logs/'+req.body.user+'.txt', save, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+        }
+        j=markers.length;
+      } 
+      else {
+        if(req.body.time!=null && req.body.work!=null && !req.body.work && j+1==markers.length){
+          var save="User: "+req.body.user+". Out of work: "+req.body.time+"\n";
+          fs.appendFile('users logs/'+req.body.user+'.txt', save, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+        }
+      }
+      j++
     }
   }
-  //fs.writeFileSync('position.json', JSON.stringify(req.body));
   res.send(true);
-})
+});
 
- app.listen(3000, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
